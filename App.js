d6d1a669f8e767ad4fc6904ae7bb2ec3d22b2b82
Muruganandham <63532028+AnandMurugan5@users.noreply.github.com>
2020-09-06 01:51:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './App.css';
+import './bootstrap.css';
+import './firebase.js'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>welcome to <b>frendship</b> blog</h1>
+        <p> A college (Latin: collegium) is an educational institution or a constituent part of one. <br></br>
+       <span>A college may be a degree-awarding tertiary educational institution, a part of a collegiate or federal university, an institution offering vocational education, or a secondary school. 
+       </span>
+    </p>
+    <p>
+      <h2>Friendship</h2> is a relationship of mutual affection between people.
+       It is a stronger form of interpersonal bond than an association, and has been studied in academic fields such as communication, sociology, social psychology, anthropology, and philosophy. 
+       Various academic theories of friendship have been proposed, including social exchange theory, equity theory, relational dialectics, and attachment styles.
+      Although there are many forms of friendship, some of which may vary from place to place, certain characteristics are present in many types of such bonds. 
+      Such characteristics include affection; kindness, love, virtue, sympathy, empathy, honesty, altruism, loyalty, generosity, forgiveness, mutual understanding and compassion, enjoyment of each other's company, trust, and the ability to be oneself, express one's feelings to others, and make mistakes without fear of judgment from the friend. 
+      Friendship is an essential aspect of relationship building skills. 
+      </p>
+      
+<p><h2>Childhood friends</h2>
+
+The understanding of friendship in children tends to be more heavily focused on areas such as common activities, physical proximity, and shared expectations.
+These friendships provide opportunity for playing and practicing self-regulation. Most children tend to describe friendship in terms of things like sharing, and children are more likely to share with someone they consider to be a friend.
+As children mature, they become less individualized and are more aware of others. They gain the ability to empathize with their friends, and enjoy playing in groups. They also experience peer rejection as they move through the middle childhood years. 
+Establishing good friendships at a young age helps a child to be better acclimated in society later on in their life.
+Based upon the reports of teachers and mothers, 75% of preschool children had at least one friend. 
+This figure rose to 78% through the fifth grade, as measured by co-nomination as friends, and 55% had a mutual best friend.
+ About 15% of children were found to be chronically friendless, reporting periods without mutual friends at least six months.
+Potential benefits of friendship include the opportunity to learn about empathy and problem solving.
+Coaching from parents can be useful in helping children to make friends. 
+Eileen Kennedy-Moore describes three key ingredients of children's friendship formation:
+<ul> 
+ <ol>
+   <li>openness</li> 
+   <li> similarity</li>
+   <li>shared fun</li>
+  </ol></ul>
+Parents can also help children understand social guidelines they haven't learned on their own.
+<span> "I Want It My Way", "What's In It For Me?", "By the Rules", "Caring and Sharing", and "Friends Through Thick and Thin."</span>
+</p>
+
+  <p align='center'>
+    <b>This blog created by<br></br>
+    Anand
+    </b>
+  </p>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDYKkEIVvM5Q7k6LT7XK-CL3o5IKIdbkTM",
+    authDomain: "blog-4f918.firebaseapp.com",
+    databaseURL: "https://blog-4f918.firebaseio.com",
+    projectId: "blog-4f918",
+    storageBucket: "blog-4f918.appspot.com",
+    messagingSenderId: "1003601191267",
+    appId: "1:1003601191267:web:0ccd2d06050f6c4024b356",
+    measurementId: "G-WFWZ4563GB"
+  };
